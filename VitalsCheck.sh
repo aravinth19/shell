30b34fc581fc39ef7fe29226962b718ea0221098
Aravinth Shanmugam <42541328+aravinth19@users.noreply.github.com>
2023-05-27 11:23:59 +0530

    Add files via upload

--- VitalsCheck.sh
+++ VitalsCheck.sh
@@ -1,5 +1,4 @@
 #!bin/bash
-
 #Configuration
 THRESHOLD_CPU=80
 THRESHOLD_MEM=80
@@ -11,19 +10,14 @@ DB_USERNAME="bn_opencart"
 DB_PASSWORD="650c992f26adb8a14252d7c5f3182ef79aea9cd578f69350315e1cec7813a50e"
 DB_DATABASE="bitnami_opencart"
 DB_PORT=3306
-
 #Set status check variable
 vitals_passed=true
-
 #Check CPU Usage of EC2 Instance
 top
-
 #Check Memory Usage of EC2 Instance
 free
-
 #Check Disk Space Usage of EC2 Instance
 df
-
 #Check Status of Bitnami Service
 #Check Status of MYSQL Service
 #Set the Status for Automation Runs
